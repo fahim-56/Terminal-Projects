28554a8bee8b6eb2416480f2b66f483f529bfbea
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-11-24 02:13:12 +0600

    Add files via upload
    Basic Shopping Terminal Project

---
+++ Shoping Project.py
@@ -0,0 +1,71 @@
+class product:
+    product_list = []
+
+    def __init__(self):
+        pass
+       
+    def add_to_list(self,name,price_per_unit,total_quantity):
+        self.name = name
+        self.price_per_unit = price_per_unit
+        self.total_quantity = total_quantity
+
+        self.product_list.append({"name":self.name,"price":self.price_per_unit,"quantity":self.total_quantity})
+
+class shope(product):
+
+    cart = []
+
+    def __init__(self):
+        super().__init__()
+
+    def Add_to_cart(self,item,quantity):
+        self.item = item
+        self.quantity = quantity
+        flag = True
+        for product in self.product_list:
+            if(product["name"] == self.item ):
+                flag = False
+                if(product["quantity"]>=self.quantity ):
+                    self.cart.append({"item": product["name"],"price": (self.quantity * product["price"] )})
+                    product["quantity"] -= self.quantity
+                    print(f"{self.quantity} unit {product["name"]} added to cart..")
+                else:
+                    print(f"Sorry, {product["quantity"]}  unit  {product["name"]} is available just.")
+        if(flag):
+            print("The Product is not available here.")
+
+    def Remove_the_last_item_From_cart(self):
+        self.cart.pop()
+
+    def checkout(self):
+        Total_price = 0
+        for x in self.cart:
+            Total_price += x["price"]
+
+        self.cart.clear()
+        print(f'\nPlease pay {Total_price} Taka')
+
+    
+
+# Include product into shop
+ 
+products = product()
+products.add_to_list("Sugar",120,20)
+products.add_to_list("Salt",40,10)
+products.add_to_list("Rice",70,50)
+products.add_to_list("Oil",180,5)
+products.add_to_list("Harpic",120,30)
+
+
+# shoping
+
+customer = shope()
+customer.Add_to_cart("Sugar",5)
+customer.Add_to_cart("Rice",10)
+customer.Add_to_cart("Salt",10)
+customer.Add_to_cart("Salt",10)
+customer.Add_to_cart("Oil",10)
+customer.Add_to_cart("Egg",10)
+
+customer.Remove_the_last_item_From_cart()
+customer.checkout()
